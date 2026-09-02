d7468fd7887911e8b286712371ce1a1ac222302c
suri <surisaddai03@gmail.com>
2026-09-01 20:37:31 -0500

    Agregar try-catch al juego

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 import java.util.Random; 
 
 public class Main {
@@ -17,11 +18,10 @@ public class Main {
 
         System.out.println("Bienvenido al juego de adivinar el número, dijite un número del 1 al 100");
         
-        //Aquí el usuario le da el valor que cosidere que vaya de 1 a 100
         usuario = inicio.nextInt();
 
         // Se crea el bucle del juego, siempre y cuando el número del usuario sea distinto al número 
-        // que dió el sistema entonces el ciclo no va a terminar 
+        // que dió el sistema entonces el ciclo no va a terminar
         while(usuario != numero){
 
             if(usuario < numero){
@@ -36,13 +36,24 @@ public class Main {
                 System.out.println("No está entre las opciones");
             }
 
-            usuario = inicio.nextInt();
+            // Intentamos leer el siguiente intento del usuario
+            try {
+             usuario = inicio.nextInt();
+            } 
+            catch (InputMismatchException e) {
+            System.out.println("¡Error! Debes ingresar un número, las letras no son válidas.");
+
+            // IMPORTANTE: Limpia la letra incorrecta del búfer para evitar un bucle infinito
+            inicio.next(); 
+
+            // Asignamos un valor temporal que no afecte la lógica para que el bucle continúe
+            usuario = -1;  
+            }
+
         }
         
         System.out.println("¡FELICIDADES ADIVINASTE!");
 
-
-
-
+        
     }          
-}
+    }
