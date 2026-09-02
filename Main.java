a1aea871915984f5a7bc9449dec22dcdf9150371
suri <surisaddai03@gmail.com>
2026-09-01 19:04:01 -0500

    Creación del juego primera parte

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.Random; 
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        Scanner inicio = new Scanner(System.in);
+         
+        Random aleatorio = new Random();
+
+        int numero = aleatorio.nextInt(100) + 1;
+        int usuario;
+
+        System.out.println("Bienvenido al juego de adivinar el número, dijite un número del 1 al 100");
+        
+
+    }           
+}
