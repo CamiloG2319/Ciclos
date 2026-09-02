f75ef1b0f8f686223a4768bb2473446d64411783
suri <surisaddai03@gmail.com>
2026-09-01 23:40:41 -0500

    Agregar comentarios explicando el programa de Tablas

--- Tablas.java
+++ Tablas.java
@@ -5,10 +5,13 @@ public class Tablas {
     public static void main(String[] args) {
     Scanner inicio = new Scanner(System.in);
 
+    // Inicializar la variable 
     int numero;
     
     System.out.println("Elije un número al que desees conocer su tabla de multiplicar");
     
+    // Se agrega el Try catch para que encuentre algún error
+    // u opción no válida para este ejercicio 
             try {
                 numero = inicio.nextInt();
 
@@ -17,6 +20,9 @@ public class Tablas {
                 }
             }
             catch (InputMismatchException e) {
+
+            // Este es el texto que lanza cuando el dato ingresado no 
+            // Corresponde a algún número natural
             System.out.println("¡Error! Opción no valida.");
             }
     }
